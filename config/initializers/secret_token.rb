424f7adf7808d0b2b2d66e9422c5c047ad342dbe
Jeff Saracco <jeffsaracco@gmail.com>
2012-08-09 18:14:55 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tailspotting::Application.config.secret_token = '435f428f1ef63f5b99c7193b9a6e146883997fe2e8908a4562d57d3b28dfdb3169966bf125c2cb04f2206bf028bfca8920c3fa63b7c238ceeb362c81bfba0277'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tailspotting::Application.config.session_store :cookie_store, key: '_tailspotting_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tailspotting::Application.config.session_store :active_record_store
